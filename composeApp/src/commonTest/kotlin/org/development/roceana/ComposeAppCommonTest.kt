d152af8a94f013461f7d0632c49588072814f7d6
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2025-08-17 03:55:26 +0700

    initial commit

---
+++ composeApp/src/commonTest/kotlin/org/development/roceana/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package org.development.roceana
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/development/roceana/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.development.roceana
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
